da83a720b107ab7ace881e468a7ff13fe9f66e1a
vagrant <vagrant@homestead>
2018-03-31 19:23:53 +0000

    完成布局和路由

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-// 项目首页
+// 项目首页-渲染
 Route::get('/', 'StaticPagesController@home')->name('home');
-// 帮助页
+// 帮助页-渲染
 Route::get('/help', 'StaticPagesController@help')->name('help');
-// 关于页
+// 关于页-渲染
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+// 用户注册页-渲染
+Route::get('signup', 'UsersController@create')->name('signup');
+
